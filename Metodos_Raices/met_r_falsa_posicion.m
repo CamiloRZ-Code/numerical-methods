52942791520313b4b52d292bdccbfc72cac7d5d1
camilo-rodriguez <camiloarodriguezzacademico@gmail.com>
2024-09-24 21:04:25 -0500

    se realizo el codigo correcto del metodo de secante y se hicieron modificaciones ligeras a los otros codigos

--- Metodos_Raices/met_r_falsa_posicion.m
+++ Metodos_Raices/met_r_falsa_posicion.m
@@ -15,13 +15,13 @@ fxn = inline(fx);
 fi = fxn(xi); % se inicializa la variable que guarda el valor de la función en el extremo inferior del intervalo.
 fs = fxn(xs); % se inicializa la variable que guarda el valor de la función en el extremo superior del intervalo.
 if sign(fi)~=sign(fs) % esto hace una comprobación del teo. de Bolzano (f(a)*f(b)<0) con los valores iniciales.
-    Ea = 1; % Error relativo porc. aproximado actual
+    Ea = 1; % Se inicializa el Error relativo aproximado actual como 100%.
     ea = input('Ingrese el valor de error aceptable: '); % se recibe un Error aceptable por el usuario.
     %ea = 0.0001; % Error aceptable predefinido.
     i=1; % contador de pasos que empieza en 1 por que esos son los valores iniciales dados por el usuario.
     while Ea>ea
         xm = xs-((fs*(xi-xs))/(fi-fs)); % variable que guarda el punto medio actual.
-        fm = fxn(xm);
+        fm = fxn(xm); % variable que guarda el valor de la función en el punto.
         if fi*fm<0 
             Ea=abs((xm-xs)/xm);
             xs=xm;
@@ -33,7 +33,7 @@ if sign(fi)~=sign(fs) % esto hace una comprobación del teo. de Bolzano (f(a)*f(
         end
         i=i+1;
     end
-    resp=['La raiz es xm =' num2str(xm)]; % imprime el mensaje
+    resp=['La raiz es xm = ' num2str(xm)]; % imprime el mensaje
     disp(resp);
     ite=['El numero de iteraciones fueron: ' num2str(i)]; % imprime el mensaje
     disp(ite);
--- Metodos_Raices/met_r_secante.m
+++ Metodos_Raices/met_r_secante.m
@@ -7,36 +7,36 @@ fx = input('Ingrese su funcion en terminos de x: '); % se recibe la función del
 x1 = input ('Ingrese valor inferior intervalo a graficar: '); % extremo inferior de la grafica.
 x2 = input ('Ingrese valor superior intervalo a graficar: '); % extremo superior de la grafica.
 fplot(fx,[x1,x2]); grid on; %imprime la grafica.
-xi = input ('Ingrese valor inferior intervalo (a): '); % se inicializa la variable de extremo inferior con valor inicial pasado por el usuario.
-xs = input ('Ingrese valor superior intervalo (b): '); % se inicializa la variable de extremo superior con valor inicial pasado por el usuario.
+xi = input ('Ingrese valor inferior intervalo: '); % se inicializa la variable de valor inferior (Xi-1) con valor inicial pasado por el usuario.
+xs = input ('Ingrese valor superior intervalo: '); % se inicializa la variable de valor superior (Xi) con valor inicial pasado por el usuario.
 
 % Se evalua el algoritmo con los extremos iniciales:
 fxn = inline(fx);
 fi = fxn(xi); % se inicializa la variable que guarda el valor de la función en el extremo inferior del intervalo.
 fs = fxn(xs); % se inicializa la variable que guarda el valor de la función en el extremo superior del intervalo.
-if sign(fi)~=sign(fs) % esto hace una comprobación del teo. de Bolzano (f(a)*f(b)<0) con los valores iniciales.
-    Ea = 1; % Error relativo porc. aproximado actual
-    ea = input('Ingrese el valor de error aceptable: '); % se recibe un Error aceptable por el usuario.
-    %ea = 0.0001; % Error aceptable predefinido.
-    i=1; % contador de pasos que empieza en 1 por que esos son los valores iniciales dados por el usuario.
-    while Ea>ea
-        xm = xs-((fs*(xi-xs))/(fi-fs)); % variable que guarda el punto medio actual.
-        fm = fxn(xm);
-        if fi*fm<0 
-            Ea=abs((xm-xs)/xm);
-            xs=xm;
-            fs=fm;
-        else
-            Ea=abs((xm-xi)/xm);
-            xi=xm;
-            fi=fm;
-        end
-        i=i+1;
+Ea = 1; % Se inicializa el Error relativo aproximado actual como 100%.
+ea = input('Ingrese el valor de error aceptable: '); % se recibe un Error aceptable por el usuario.
+%ea = 0.0001; % Error aceptable predefinido.
+li = input('Ingrese el numero de iteraciones maximas aceptable: ');
+i=1; % contador de pasos que empieza en 1 por que esos son los valores iniciales dados por el usuario.
+cent = 0; %centinela o flag que sirve para terminar el ciclo.
+while cent==0
+    xsig = xs-((fs*(xi-xs))/(fi-fs)); % variable que guarda el punto siguiente o Xi+1.
+    Ea = abs((xsig-xs)/xsig);
+    i=i+1;
+    if Ea<ea
+        cent=1;
+        resp=['La raiz es xm = ' num2str(xsig)]; % imprime el mensaje
+        disp(resp);
+        ite=['El numero de iteraciones fueron: ' num2str(i)]; % imprime el mensaje
+        disp(ite);
+    elseif i>li
+        cent=1;
+        disp('no se encontro la raíz.');
+    else
+        xi = xs; % se define la siguiente Xi-1 como la actual Xi.
+        xs = xsig; % se define la siguiente Xi como la actual Xi+1.
+        fi = fs; % se define la siguiente f(Xi-1) como la actual f(Xi).
+        fs = fxn(xsig); % se define la siguiente f(Xi) como la actual f(Xi+1).
     end
-    resp=['La raiz es xm =' num2str(xm)]; % imprime el mensaje
-    disp(resp);
-    ite=['El numero de iteraciones fueron: ' num2str(i)]; % imprime el mensaje
-    disp(ite);
-else
-    disp('Error, No hay raiz en el intervalo');
 end
